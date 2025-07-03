ccd8d260a863cb32f1d5946353260ca702f55b17
amantewari <amantewari1836@gmail.com>
2025-07-03 22:29:12 +0530

    Update Employee.java

--- src/main/java/com/example/employeeservice/model/Employee.java
+++ src/main/java/com/example/employeeservice/model/Employee.java
@@ -1,9 +1,39 @@
 package com.example.employeeservice.model;
 
 public class Employee {
-  private long id;
-  private String name;
-  private String role;
-  // getters/setters
-}
+    private long id;
+    private String name;
+    private String role;
 
+    public Employee() {}
+
+    public Employee(long id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
